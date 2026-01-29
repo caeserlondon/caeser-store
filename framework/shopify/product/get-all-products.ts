1e4cd4a460e8ef1745972a57744803b7a185cae5
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-01-29 21:25:17 +0700

    Update get all product

---
+++ framework/shopify/product/get-all-products.ts
@@ -0,0 +1,22 @@
+import { ApiConfig } from '@common/types/api';
+import { Product } from '@common/types/product';
+import { getAllProductsQuery, normalizeProduct } from '@framework/utils';
+import { ProductConnection } from '../schema';
+
+type ReturnType = {
+	products: ProductConnection;
+};
+
+const getAllProducts = async (config: ApiConfig): Promise<Product[]> => {
+	const { data } = await config.fetch<ReturnType>({
+		query: getAllProductsQuery,
+	});
+
+	const products =
+		data.products.edges.map(({ node: product }) => normalizeProduct(product)) ??
+		[];
+
+	return products;
+};
+
+export default getAllProducts;
