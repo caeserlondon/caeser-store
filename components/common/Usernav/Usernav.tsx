aa30b2b3b96d64cf0a513100f0a953ef7378a82e
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-02-01 19:14:25 +0700

    Add the user nav

---
+++ components/common/Usernav/Usernav.tsx
@@ -0,0 +1,35 @@
+import { LineItem } from '@common/types/cart';
+import { Bag as Cart, Heart } from '@components/icons';
+import { useUI } from '@components/ui/context';
+import useCart from '@framework/cart/use-cart';
+import Link from 'next/link';
+import { FC } from 'react';
+import s from './Usernav.module.css';
+
+const Usernav: FC = () => {
+	const { openSidebar } = useUI();
+	const { data } = useCart();
+
+	const itemsCount =
+		data?.lineItems.reduce((count: number, item: LineItem) => {
+			return count + item.quantity;
+		}, 0) ?? 0;
+
+	return (
+		<nav>
+			<ul className={s.list}>
+				<li className={s.item}>
+					<Cart onClick={openSidebar} />
+					{itemsCount > 0 && <span className={s.bagCount}>{itemsCount}</span>}
+				</li>
+				<li className={s.item}>
+					<Link href='/wishlist'>						
+							<Heart />						
+					</Link>
+				</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default Usernav;
