eb8680dd972cc2a38343f3caaca065a2fa09f99c
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-02-02 20:10:12 +0700

    Add the marquee

---
+++ components/ui/Marquee/Marquee.tsx
@@ -0,0 +1,25 @@
+import cn from 'classnames';
+import { FC, ReactNode } from 'react';
+import Ticker from 'react-ticker';
+import s from './Marquee.module.css';
+
+interface Props {
+	children: ReactNode[];
+	variant?: 'primary' | 'secondary';
+}
+
+const Marquee: FC<Props> = ({ children, variant = 'primary' }) => {
+	const rootClassName = cn(s.root, {
+		[s.secondary]: variant === 'secondary',
+	});
+
+	return (
+		<div className={rootClassName}>
+			<Ticker offset={80}>
+				{() => <div className={s.container}>{children}</div>}
+			</Ticker>
+		</div>
+	);
+};
+
+export default Marquee;
