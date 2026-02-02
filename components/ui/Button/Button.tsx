1d5fdf752997c22a24d4132498bd9453d6fcaa32
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-02-02 20:09:30 +0700

    Update the button

---
+++ components/ui/Button/Button.tsx
@@ -0,0 +1,42 @@
+import { LoadingDots } from '@components/ui';
+import cn from 'classnames';
+import {
+	ButtonHTMLAttributes,
+	ComponentType,
+	FC,
+	HTMLAttributes,
+	ReactNode,
+} from 'react';
+import s from './Button.module.css';
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+	children: ReactNode | ReactNode[];
+	isLoading?: boolean;
+	Component?: string | ComponentType<HTMLAttributes<HTMLElement>>;
+	href?: string;
+}
+
+const Button: FC<Props> = ({
+	children,
+	className,
+	isLoading = false,
+	Component = 'button',
+	...rest
+}) => {
+	const rootClassName = cn(s.root, className, {
+		[s.loading]: isLoading,
+	});
+
+	return (
+		<Component className={rootClassName} type='button' {...rest}>
+			{children}
+			{isLoading && (
+				<i className='pl-2 m-0 flex'>
+					<LoadingDots />
+				</i>
+			)}
+		</Component>
+	);
+};
+
+export default Button;
