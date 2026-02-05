c838fc30d3be9cd74a24bdf7bbd64e010329e2a7
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-02-05 21:15:01 +0700

    Add the index

---
+++ components/ui/index.ts
@@ -0,0 +1,7 @@
+export { default as Button } from './Button';
+export { default as Container } from './Container';
+export { default as Grid } from './Grid';
+export { default as Hero } from './Hero';
+export { default as LoadingDots } from './LoadingDots';
+export { default as Marquee } from './Marquee';
+export { default as Sidebar } from './Sidebar';
