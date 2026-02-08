0aed61cea60d9e5dd8281ca2cd9cd2d97d7db9b9
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-02-08 17:43:13 +0700

    Add the index

---
+++ framework/shopify/utils/index.ts
@@ -0,0 +1,7 @@
+export { default as checkoutToCart } from "./checkout-to-cart"
+export { default as createCheckout } from "./create-checkout"
+export { default as fetchApi } from "./fetch-api"
+export { default as getCheckoutId } from "./get-checkout-id"
+
+export * from "./normalize"
+export * from "./queries"
