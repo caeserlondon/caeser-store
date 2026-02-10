6197109f8f5f2dd8b8e7b058c07afcb420995158
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-02-10 15:23:57 +0700

    Update the config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,22 +1,14 @@
-// import autoprefixer from 'autoprefixer';
-// import tailwindcss from 'tailwindcss';
 
-// export default {
-//   plugins: [tailwindcss, autoprefixer],
-// };
 
-// module.exports = {
-// 	plugins: ['tailwindcss', 'postcss-nesting', 'autoprefixer'],
-// };
 
+// postcss.config.cjs
 module.exports = {
-	plugins: {
-		tailwindcss: {},
-		autoprefixer: {},
-		'postcss-nesting': {},
-	},
+  plugins: {
+    'postcss-nesting': {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
 // For Testing to generate the "main_browser.css"
 
 // module.exports = {
