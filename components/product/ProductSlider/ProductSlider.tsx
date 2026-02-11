28c8b6b6a4a9f3b1f3b352f177f1819355117107
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-02-11 16:30:14 +0700

    Update the product slider

--- components/product/ProductSlider/ProductSlider.tsx
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -1,56 +1,59 @@
-import cn from 'classnames';
-import { useKeenSlider } from 'keen-slider/react';
-import React, { Children, FC, isValidElement, useState } from 'react';
-import s from './ProductSlider.module.css';
 
-const ProductSlider: FC = ({ children }) => {
-	const [currentSlide, setCurrentSlide] = useState(0);
-	const [sliderRef, slider] = useKeenSlider({
-		initial: 0,
-		loop: true,
-		slideChanged(s) {
-			setCurrentSlide(s.details().relativeSlide);
-		},
-	});
+import cn from "classnames"
+import { useKeenSlider } from "keen-slider/react"
+import React, { Children, FC, isValidElement, ReactNode, useState } from "react"
+import s from "./ProductSlider.module.css"
 
-	return (
-		<div className={s.root}>
-			<div
-				ref={sliderRef as React.RefObject<HTMLDivElement>}
-				className='keen-slider h-full transition-opacity'
-			>
-				<button
-					onClick={slider?.prev}
-					className={cn(s.leftControl, s.control)}
-				/>
-				<button
-					onClick={slider?.next}
-					className={cn(s.rightControl, s.control)}
-				/>
-				{Children.map(children, (child) => {
-					if (isValidElement(child)) {
-						return {
-							...child,
-							props: {
-								...child.props,
-								className: `${
-									child.props.className ? `${child.props.className}` : ''
-								} keen-slider__slide`,
-							},
-						};
+const ProductSlider: FC<{children: ReactNode}> = ({children}) => {
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const [sliderRef, slider] = useKeenSlider({
+    initial: 0,
+    loop: true,
+    slideChanged(s) {
+      setCurrentSlide(s.details().relativeSlide)
+    },
+  })
 
-						// return React.cloneElement(child, {
-						//   className: `${
-						//     child.props.className ? `${child.props.className}` : ""
-						//   } keen-slider__slide`
-						// })
-					}
+  return (
+    <div className={s.root}>
+      <div
+        ref={sliderRef as React.RefObject<HTMLDivElement>}
+        className="keen-slider h-full transition-opacity">
+        <button
+          onClick={slider?.prev}
+          className={cn(s.leftControl, s.control)}
+					aria-labelledby="previous product"
+        />
+        <button
+          onClick={slider?.next}
+          className={cn(s.rightControl, s.control)}
+					aria-labelledby="next product"
+        />
+        { Children.map(children, child => {
+          if (isValidElement(child)) {
+            return {
+              ...child,
+              props: {
+                ...child.props,
+                className: `${
+                  child.props.className ? `${child.props.className}` : ""
+                } keen-slider__slide`
+              }
+            }
 
-					return child;
-				})}
-			</div>
-		</div>
-	);
-};
+            // return React.cloneElement(child, {
+            //   className: `${
+            //     child.props.className ? `${child.props.className}` : ""
+            //   } keen-slider__slide`
+            // })
+          }
 
-export default ProductSlider;
+          return child
+        }
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default ProductSlider
