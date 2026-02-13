a472d0e90c8653e3fa8923055524eeebfd9182d0
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-02-13 17:45:12 +0700

    Update the index

---
+++ framework/shopify/utils/queries/index.ts
@@ -0,0 +1,5 @@
+
+
+export { default as getAllProductsQuery } from "./get-all-products"
+export { default as getCheckoutQuery } from "./get-checkout"
+export { default as getProductQuery } from "./get-product"
